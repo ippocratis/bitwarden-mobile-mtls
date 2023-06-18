6e36b7699a6490874b14a08c40a416127eb93ff4
ᓰᕵᕵᓍ <52239579+ippocratis@users.noreply.github.com>
2023-06-18 15:26:55 +0300

    Create ApiExceptionTlsAuthRequired.cs

---
+++ src/Core/Exceptions/ApiExceptionTlsAuthRequired.cs
@@ -0,0 +1,18 @@
+﻿using Bit.Core.Models.Response;
+using System;
+
+namespace Bit.Core.Exceptions
+{
+    public class ApiExceptionTlsAuthRequired : ApiException
+    {
+        public ApiExceptionTlsAuthRequired() { }
+
+        public ApiExceptionTlsAuthRequired(ErrorResponse error)
+            : this()
+        {
+            Error = error;
+        }
+
+        public ErrorResponse Error { get; set; }
+    }
+}
