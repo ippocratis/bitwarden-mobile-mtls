81c3edc3bdfd9fd20d60ebb73eed439e0b55e392
ᓰᕵᕵᓍ <52239579+ippocratis@users.noreply.github.com>
2023-06-18 15:21:33 +0300

    Create IHttpMessageHandler.cs

---
+++ src/Core/Abstractions/IHttpMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Bit.Core.Models;
+
+namespace Bit.Core.Abstractions
+{
+    public interface IHttpMessageHandler
+    {
+        void UseCertificateContainer(ICertificateContainer certificateContainer);
+        HttpClientHandler AsClientHandler();
+    }
+}
