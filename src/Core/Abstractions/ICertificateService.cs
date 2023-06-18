614829734f406b7fe9c0835903ea4b4ee0dad4dc
ᓰᕵᕵᓍ <52239579+ippocratis@users.noreply.github.com>
2023-06-18 15:20:49 +0300

    Create ICertificateService.cs

---
+++ src/Core/Abstractions/ICertificateService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Bit.Core.Abstractions
+{
+    public interface ICertificateService
+    {
+        Task<bool> PickAndSaveCertificate();
+        Task ResetSelectedCertificate();
+        Task<bool> SetCertificateContainerFromStorageAsync();
+    }
+}
